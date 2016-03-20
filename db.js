9270f9725aa1862ea3259e64fc9f55dbb6515bea
Moppy <erik@eext.us>
2016-03-20 21:39:18 +0900

    Trying to upload to heroku again

--- db.js
+++ db.js
@@ -4,6 +4,8 @@ var Sequelize = require('sequelize');								// Importing
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
+console.log('Running in environment: ' + env);
+
 if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
