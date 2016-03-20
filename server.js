f842cc648114a235a6aa8aa3f8782f9b29bc7e84
Moppy <erik@eext.us>
2016-03-20 19:56:43 +0900

    Added fetching and validation

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var _ = require('underscore');	// Documentiation http://underscorejs.org/#where
+var _ = require('underscore'); // Documentiation http://underscorejs.org/#where
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -10,7 +10,7 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
@@ -26,17 +26,21 @@ app.get('/todos', function(req, res) {
 	var filteredTodos = todos;
 
 	// If has property and is completed. Note that completed is a string
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
-		filteredTodos = _.where(filteredTodos, {completed: true});
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {
+			completed: true
+		});
 
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});
+		filteredTodos = _.where(filteredTodos, {
+			completed: false
+		});
 	}
 
 	// Check if the q property exists and isn't empty
 	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
 		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1 ; // -1 indicates the result wasn't contained
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; // -1 indicates the result wasn't contained
 		});
 	}
 
@@ -45,10 +49,12 @@ app.get('/todos', function(req, res) {
 
 // GET /todos/:id (the : represents a variable that will be passed in)
 app.get('/todos/:id', function(req, res) {
-	var todoId = parseInt(req.params.id, 10);	// The request value comes in as a string so it has to be converted to a base 10 number
-	
+	var todoId = parseInt(req.params.id, 10); // The request value comes in as a string so it has to be converted to a base 10 number
+
 	// Filter for the object with that id. See http://underscorejs.org/#findWhere
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -58,15 +64,15 @@ app.get('/todos/:id', function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', function(req, res){
-	
+app.post('/todos', function(req, res) {
+
 	// _.pick() pulls off only the info you're interested
 	// trim() removes leading and trailing spaces
 	var body = _.pick(req.body, 'description', 'completed');
 	console.log(body);
-	
+
 	// Validate the data. 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send(); // 400 means bad data and response couldn't be completed
 	}
 
@@ -80,12 +86,16 @@ app.post('/todos', function(req, res){
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res){
-	var todoId = parseInt(req.params.id, 10);	// The request value comes in as a string so it has to be converted to a base 10 number
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10); // The request value comes in as a string so it has to be converted to a base 10 number
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
 	if (!matchedTodo) {
-		return res.status(404).json({"error": "no todo with that id"}); 
+		return res.status(404).json({
+			"error": "no todo with that id"
+		});
 	} else {
 		// _.without(array, value1, value2,...) returns an array with specified values removed
 		todos = _.without(todos, matchedTodo);
@@ -95,12 +105,14 @@ app.delete('/todos/:id', function(req, res){
 
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res){
-	
+app.put('/todos/:id', function(req, res) {
+
 	// Find the specified item
-	var todoId = parseInt(req.params.id, 10);	
-	var matchedTodo = _.findWhere(todos, {id: todoId});
-	
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
+
 	// Stop now if nothing was found
 	if (!matchedTodo) {
 		return res.status(404).send();
@@ -114,18 +126,18 @@ app.put('/todos/:id', function(req, res){
 	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
 		// Has the property and is a boolean
 		validAttributes.completed = body.completed;
-	
-	} else if (body.hasOwnProperty('completed')){
+
+	} else if (body.hasOwnProperty('completed')) {
 		// Had the proprty, but it wasn't a boolean
 		return res.status(400).send();
-	} 
+	}
 
 	// Validate description
 	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
 		validAttributes.description = body.description;
 	} else if (body.hasOwnProperty('description')) {
 		return res.status(400).send();
-	} 
+	}
 
 	// If we make it to here, everything went well
 	// .ext add/overwrites new data to an object
@@ -137,5 +149,3 @@ app.put('/todos/:id', function(req, res){
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '!');
 });
-
-
